a1bbe7ca2a9cd5eb44a44ed17926f58cc8d92cb0
Manuel Duarte  <carlosd99m@gmail.com>
2023-03-11 20:45:38 -0600

    feature: example dart frog and flutter app (#1)
    * wip: initial code
    
    * feat: dart_frog and flutter app example

---
+++ user_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:app/bootstrap.dart';
+import 'package:app/users/view/users_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:users_api/api.dart';
+import 'package:users_repository/users_repository.dart';
+
+void main() {
+  bootstrap(() async {
+    const baseUrl = 'http://localhost:7878';
+    final apiClient = ApiClient(baseUrl: baseUrl);
+
+    final usersRepository = UsersRepository(apiClient: apiClient);
+
+    print('API_URL: $baseUrl');
+
+    return App(
+      usersRepository: usersRepository,
+    );
+  });
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+    required usersRepository,
+  }) : _usersRepository = usersRepository;
+
+  final UsersRepository _usersRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: _usersRepository),
+      ],
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Users Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const UsersRoute(),
+    );
+  }
+}
--- users_api/src/api.dart
+++
@@ -1,6 +0,0 @@
-library api;
-
-export 'client/api_client.dart';
-export 'data/in_memory_users_data_source.dart';
-export 'data/models/models.dart';
-export 'data/users_data_source.dart';
